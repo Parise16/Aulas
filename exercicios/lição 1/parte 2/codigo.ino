9c10646d04ccee1f943afaeb41eb4594f5919108
Parise <99494302+Parise16@users.noreply.github.com>
2022-03-25 12:52:01 -0300

    Create codigo.ino

---
+++ exercicios/lição 1/parte 2/codigo.ino
@@ -0,0 +1,15 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(6, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(6, HIGH);  digitalWrite(5, LOW);
+  delay(1000);
+  digitalWrite(6, LOW); digitalWrite(5, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
